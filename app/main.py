c9bb4032356b6548a8e616388c7e3c3c3bf562f0
bentref <bentref11@gmx.com>
2021-05-31 11:06:41 -0700

    Return homepage stats and attempt at printer stats tested on ssh (not integration tested locally)

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2AuthorizationCodeBearer
 from fastapi.middleware.cors import CORSMiddleware
 
+import ocflib.lab.stats as labstats
+import ocflib.printing.printers as printstats
+
 # Keycloak setup
 from keycloak import KeycloakOpenID
 
@@ -11,7 +14,9 @@ keycloak_url = "https://auth.ocf.berkeley.edu/auth/"
 realm_name = "ocf"
 
 keycloak_openid = KeycloakOpenID(
-    server_url=keycloak_url, client_id="ocfapi", realm_name=realm_name,
+    server_url=keycloak_url,
+    client_id="ocfapi",
+    realm_name=realm_name,
 )
 
 app = FastAPI()
@@ -65,6 +70,58 @@ async def root():
 async def get_user(current_user: dict = Depends(get_current_user)):
     logging.info(current_user)
     return current_user
+
+
+@app.get("/stats")
+async def get_homepage_stats():
+    # users = labstats.users_in_lab_count()
+    return {
+        "Pages Printed": sum(get_pages_today().values()),
+        "Bandwidth Usage": 456,
+        "Hours Farmed": 888,
+        "Accounts Created": 13,
+    }
+
+
+def get_pages_today():
+    """Return a dict with the number of pages printed today by each printer"""
+    with labstats.get_connection() as cursor:
+        cursor.execute(
+            """
+            SELECT max(value) as value, cast(date as date) as date, printer
+                FROM printer_pages_public
+                GROUP BY cast(date as date), printer
+                ORDER BY date ASC, printer ASC
+        """
+        )
+
+        pages_today = {}
+        last_seen = {printer: 0 for printer in printstats.PRINTERS}
+
+        for row in cursor:
+            printer = row["printer"]
+            if printer in printstats.PRINTERS:
+                pages_today[printer] = row["value"] - last_seen[printer]
+                last_seen[printer] = row["value"]
+
+        return pages_today
+
+        # Stuff from ocfweb, probably not using
+
+        # ~ # Resolves the issue of possible missing dates.
+        # ~ # defaultdict(lambda: defaultdict(int))
+        # ~ doesn't work due to inability to pickle local objects like lambdas;
+        # ~ # this effectively does the same thing as that.
+        # ~ pages_printed: Dict[Any, Any] = defaultdict(partial(defaultdict, int))
+        # ~ last_seen: Dict[Any, Any] = {}
+
+        # ~ for row in cursor:
+        # ~ if row['printer'] in last_seen:
+        # ~ pages_printed.setdefault(row['date'], defaultdict(int))
+        # ~ pages_printed[row['date']][row['printer']] = (
+        # ~ row['value'] - last_seen[row['printer']]
+        # ~ )
+        # ~ last_seen[row['printer']] = row['value']
 
 
 # Some great reference reading: https://github.com/tiangolo/fastapi/issues/12
