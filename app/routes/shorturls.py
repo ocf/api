e508d0e89de01d61b2f544e980213b597907ff8b
Kian Sutarwala <kian@ocf.berkeley.edu>
2022-03-06 11:29:29 -0800

    Update shorturl endpoint

--- app/routes/shorturls.py
+++ app/routes/shorturls.py
@@ -1,18 +1,21 @@
 from ocflib.misc.shorturls import get_connection
 from ocflib.misc.shorturls import get_shorturl
 
-from fastapi import Response, status
+from fastapi import status
 from fastapi.responses import RedirectResponse
+from fastapi.exceptions import HTTPException
 from . import router
 
 
 @router.get("/shorturl/{slug}", status_code=status.HTTP_301_MOVED_PERMANENTLY)
-async def bounce_shorturl(slug: str, response: Response):
+async def bounce_shorturl(slug: str):
     if slug:
         with get_connection() as ctx:
             target = get_shorturl(ctx, slug)
 
         if target:
-            return RedirectResponse(target)
+            return RedirectResponse(
+                target, status_code=status.HTTP_301_MOVED_PERMANENTLY
+            )
 
-    response.status_code = status.HTTP_404_NOT_FOUND
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
