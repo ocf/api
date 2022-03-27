5827af357f2e9162411393b0c31db984df85f7af
Kian Sutarwala <kian@ocf.berkeley.edu>
2022-03-26 17:21:02 -0700

    Fix JWT validation

--- app/utils/calnet.py
+++ app/utils/calnet.py
@@ -1,7 +1,7 @@
 import re
 from fastapi import Depends, HTTPException, status
-from fastapi.security.http import HTTPBearer
-from jose import jwt
+from fastapi.security.http import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
 from time import time
 from math import floor
 from typing import Dict, Union
@@ -10,7 +10,9 @@ from utils.constants import API_HOST
 from urllib.parse import urljoin
 from utils.config import get_settings
 
-__JWT_SECRET = os.getrandom(32).hex()
+settings = get_settings()
+
+__JWT_SECRET = os.getrandom(32).hex() if not settings.debug else "waddles"
 JWT_AUDIENCE = "ocfapi_calnet"
 
 calnet_jwt_auth_scheme = HTTPBearer(
@@ -20,51 +22,81 @@ calnet_jwt_auth_scheme = HTTPBearer(
 )
 
 
-def get_calnet_uid(calnet_jwt: str = Depends(calnet_jwt_auth_scheme)) -> int:
-    payload = decode_calnet_jwt(calnet_jwt)
-    if not verify_calnet_jwt(payload):
+def get_calnet_uid(
+    calnet_jwt: HTTPAuthorizationCredentials = Depends(calnet_jwt_auth_scheme),
+) -> int:
+    try:
+        payload = decode_calnet_jwt(calnet_jwt.credentials)
+        if not verify_calnet_jwt(payload):
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED, "malformed jwt")
+    except JWTError as e:
+        print(e)
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, "malformed jwt")
-    return int(payload["sub"])
+    else:
+        return int(payload["sub"])
 
 
 def create_calnet_jwt(uid: Union[int, str]) -> str:
     current_time = floor(time())
     return jwt.encode(
         {
+            "aud": JWT_AUDIENCE,
             "sub": str(uid),
             "iat": current_time,
             "exp": current_time + 60 * 30,  # 60 sec * 30 min
         },
         __JWT_SECRET,
+        algorithm="HS256",
     )
 
 
 def verify_calnet_jwt(payload: Dict[str, str]) -> bool:
-    if "aud" not in payload or payload["aud"] != JWT_AUDIENCE:
-        return False
-    if (
-        "iat" not in payload
-        or not payload["iat"].isdigit()
-        or float(payload["iat"]) > time()
-    ):
-        return False
-    if (
-        "exp" not in payload
-        or not payload["exp"].isdigit()
-        or float(payload["exp"]) < time()
-    ):
-        return False
-    if "sub" not in payload or not payload["sub"].isdigit():
+    try:
+        if payload["aud"] != JWT_AUDIENCE:
+            return False
+        if float(payload["iat"]) > time():
+            return False
+        if float(payload["exp"]) < time():
+            return False
+        if not int(payload["sub"]):
+            return False
+    except Exception:
+        # will catch fields not existing, or not in correct type
         return False
     return True
 
 
 def decode_calnet_jwt(calnet_jwt: str) -> Dict[str, str]:
-    return jwt.decode(calnet_jwt, __JWT_SECRET)
+    # the verification through python-jose is
+    # not as good as what we can do ourselves
+    return jwt.decode(
+        calnet_jwt,
+        __JWT_SECRET,
+        algorithms="HS256",
+        options={
+            "verify_signature": True,
+            "verify_aud": False,
+            "verify_iat": False,
+            "verify_exp": False,
+            "verify_nbf": False,
+            "verify_iss": False,
+            "verify_sub": False,
+            "verify_jti": False,
+            "verify_at_hash": True,
+            "require_aud": False,
+            "require_iat": False,
+            "require_exp": False,
+            "require_nbf": False,
+            "require_iss": False,
+            "require_sub": False,
+            "require_jti": False,
+            "require_at_hash": False,
+            "leeway": 0,
+        },
+    )
 
 
 def get_calnet_service_url(host: str = API_HOST) -> str:
-    settings = get_settings()
     if not re.match("^https?://", host):
         if settings.debug:
             host = "http://" + host
