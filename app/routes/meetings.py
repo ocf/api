c807ea115838e9b0bc2447f752ea57ae44f546a2
Kian Sutarwala <kian@ocf.berkeley.edu>
2022-04-02 14:02:43 -0700

    add meetings types

--- app/routes/meetings.py
+++ app/routes/meetings.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from ocflib.org.meeting_hours import (
     read_current_meeting,
     read_meeting_list,
@@ -5,16 +7,30 @@ from ocflib.org.meeting_hours import (
 )
 
 from fastapi import Response, status
+from pydantic import BaseModel
 
 from routes import router
 
 
-@router.get("/meetings/list", tags=["meetings"])
+class MeetingOutput(BaseModel):
+    day: str
+    time: str
+    subject: str
+    short: str
+    irl: bool
+    virtual: bool
+
+
+class MeetingsListOutput(BaseModel):
+    meetings: List[MeetingOutput]
+
+
+@router.get("/meetings/list", tags=["meetings"], response_model=MeetingsListOutput)
 def get_meetings_list():
-    return [item._asdict() for item in read_meeting_list()]
+    return {"meetings": [item._asdict() for item in read_meeting_list()]}
 
 
-@router.get("/meetings/next", tags=["meetings"])
+@router.get("/meetings/next", tags=["meetings"], response_model=MeetingOutput)
 def get_next_meeting():
     next_meeting = read_next_meeting()
     if next_meeting is None:
@@ -23,7 +39,7 @@ def get_next_meeting():
     return next_meeting._asdict()
 
 
-@router.get("/meetings/current", tags=["meetings"])
+@router.get("/meetings/current", tags=["meetings"], response_model=MeetingOutput)
 def get_current_meeting():
     current_meeting = read_current_meeting()
     if current_meeting is None:
