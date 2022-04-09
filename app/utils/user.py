58bb3e556289f632ccfe21f4f7d144c598869cbf
Kian Sutarwala <kian@ocf.berkeley.edu>
2022-04-09 12:07:24 -0700

    Add get group account dependent function

--- app/utils/user.py
+++ app/utils/user.py
@@ -1,5 +1,7 @@
 import logging
 
+from ocflib.account.search import user_is_group
+
 from fastapi import Depends, HTTPException, status
 
 from utils.auth import UserToken, decode_token, oauth2_scheme
@@ -15,3 +17,15 @@ async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserToken:
             detail="Invalid authentication credentials",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
+
+async def get_current_group_user(
+    user_token: UserToken = Depends(get_current_user),
+) -> UserToken:
+    if not user_is_group(user_token["preferred_username"]):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Account is not a group",
+        )
+
+    return user_token
